8d6bf8a69a42907d71dc71f1059712aefc390c93
christianpabon <ccpabon@bancolombia.com.co>
2026-01-14 14:41:40 -0500

    first commit

---
+++ applications/app-service/src/main/java/co/com/attetioncore/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.attetioncore.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.attetioncore.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
